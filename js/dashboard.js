00ade81da11cde7763387e4d87e41063896a1460
dina_khkalini <dina_khkalini@bitbucket.org>
2017-05-09 13:34:05 +0300

    dashboard

---
+++ js/dashboard.js
@@ -0,0 +1,31 @@
+$(function() {
+	var remember;
+	var id;
+		$('.ui-draggable').draggable({
+			opacity:0.9,// opacity whan you drag
+			containment:"#dashboard",
+			cancel: "button",//extention for drag
+			start: function() {
+				 remember = $(this).html();
+				 id = $(this).attr('id');
+        	}
+		});
+		$('.droppable').droppable({
+		   accept: '.ui-draggable',
+		   hoverClass: "ui-state-highlight",
+		   drop: function(event, ui) {
+				ui.draggable.remove();
+				$(this).append(`<div id=` + id + ` class=" dashboard-issue ui-draggable" ></div>`);
+				   $('#' + id).html(remember);
+				   $('#' + id).draggable({
+						opacity:0.9,
+					    containment:"#dashboard",
+						cancel: "button",//extention for drag
+						start: function() {
+							 remember = $(this).html();
+							 id = $(this).attr('id');
+						}
+				   });
+		   }
+		});
+});
